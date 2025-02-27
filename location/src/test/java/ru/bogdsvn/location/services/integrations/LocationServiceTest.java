1c97eee2356e9832f1377d4de722f216f9bdd5f7
Feigla <bogdansimon0v@mail.ru>
2025-02-27 18:54:07 +0300

    added h2 database for integration tests

--- location/src/test/java/ru/bogdsvn/location/services/integrations/LocationServiceTest.java
+++ location/src/test/java/ru/bogdsvn/location/services/integrations/LocationServiceTest.java
@@ -1,19 +1,18 @@
 package ru.bogdsvn.location.services.integrations;
 
 import com.google.common.geometry.S2LatLng;
-import jakarta.persistence.EntityManager;
-import lombok.extern.log4j.Log4j2;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.transaction.annotation.Transactional;
 import ru.bogdsvn.location.services.LocationService;
-import ru.bogdsvn.location.services.S2Service;
 import ru.bogdsvn.location.store.entities.LocationEntity;
 import ru.bogdsvn.location.store.entities.ProfileEntity;
 import ru.bogdsvn.location.store.repositories.LocationRepository;
@@ -23,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ExtendWith(SpringExtension.class)
+@TestPropertySource(locations = "classpath:application-test.properties")
 @SpringBootTest
 public class LocationServiceTest {
     @Autowired
