6f72131167b163b6f84348304f6dc7627c2a6f12
Feigla <bogdansimon0v@mail.ru>
2025-03-02 14:29:05 +0300

    filled method getNearbyProfiles for grpc

--- location/src/main/java/ru/bogdsvn/location/services/grpc/GrpcProximityServerService.java
+++ location/src/main/java/ru/bogdsvn/location/services/grpc/GrpcProximityServerService.java
@@ -5,7 +5,12 @@ import lombok.RequiredArgsConstructor;
 import net.devh.boot.grpc.server.service.GrpcService;
 import ru.bogdsvn.grcp.proximity.Proximity;
 import ru.bogdsvn.grcp.proximity.ProximityServiceGrpc;
+import ru.bogdsvn.location.dtos.UserDto;
 import ru.bogdsvn.location.services.LocationService;
+import ru.bogdsvn.location.store.entities.ProfileEntity;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @GrpcService
@@ -14,6 +19,15 @@ public class GrpcProximityServerService extends ProximityServiceGrpc.ProximitySe
 
     @Override
     public void getNearbyProfiles(Proximity.ProfileList.Profile request, StreamObserver<Proximity.ProfileList> responseObserver) {
+        List<UserDto> users = locationService.getNearbyUsers(request.getUserId());
 
+        Proximity.ProfileList.Builder builder = Proximity.ProfileList.newBuilder();
+
+        for (UserDto user : users) {
+            builder.addProfile(Proximity.ProfileList.Profile.newBuilder().setUserId(user.getId()).build());
+        }
+
+        responseObserver.onNext(builder.build());
+        responseObserver.onCompleted();
     }
 }
