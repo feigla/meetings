d74f42ae9c15de0a889e4525613c4ce1173d4375
Feigla <bogdansimon0v@mail.ru>
2025-03-04 18:45:02 +0300

    added integration test

---
+++ recommendation engine/src/test/java/ru/bogdsvn/recommendation/services/integrations/RecommendationServiceTest.java
@@ -0,0 +1,50 @@
+package ru.bogdsvn.recommendation.services.integrations;
+
+import net.devh.boot.grpc.client.autoconfigure.GrpcClientAutoConfiguration;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import ru.bogdsvn.recommendation.dtos.ResultDto;
+import ru.bogdsvn.recommendation.services.RecommendationService;
+
+import java.util.List;
+
+@ExtendWith(SpringExtension.class)
+@TestPropertySource("classpath:application-test.properties")
+@SpringBootTest
+@ImportAutoConfiguration(exclude = GrpcClientAutoConfiguration.class)
+public class RecommendationServiceTest {
+    @SpyBean
+    private RecommendationService recommendationService;
+
+    @Test
+    void givenProfileId_shouldSaveViewedProfile() {
+        Mockito.doReturn(List.of(
+                        ResultDto.builder()
+                                .id(2L)
+                                .gender("MALE")
+                                .age(19)
+                                .name("Oleg")
+                                .description("I'm 1 man")
+                                .dist(100.1)
+                                .build(),
+                        ResultDto.builder()
+                                .id(3L)
+                                .gender("MALE")
+                                .age(20)
+                                .name("Vladislav")
+                                .description("I'm 2 man")
+                                .dist(105.1)
+                                .build()
+                )
+        ).when(recommendationService).fetchUsers(1);
+        recommendationService.getRecommendation(1);
+        Assertions.assertEquals(1, 1);
+    }
+}
