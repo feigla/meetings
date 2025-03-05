c6790c4c0190bf9d338a72e8db38d7010c806f72
Feigla <bogdansimon0v@mail.ru>
2025-03-05 15:10:31 +0300

    added handing validation and constraint violation exceptions

--- profile/src/main/java/ru/bogdsvn/profile/errors/GlobalExceptionHandler.java
+++ profile/src/main/java/ru/bogdsvn/profile/errors/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package ru.bogdsvn.profile.errors;
 
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -28,6 +33,28 @@ public class GlobalExceptionHandler {
                         .builder()
                         .error(HttpStatus.NOT_FOUND.getReasonPhrase())
                         .message(ex.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorDto> handleValidationException(MethodArgumentNotValidException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorDto.builder()
+                        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                        .message(ex.getBindingResult().getAllErrors().getFirst().getDefaultMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ErrorDto> handleConstraintViolationException(ConstraintViolationException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorDto.builder()
+                        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                        .message(ex.getConstraintViolations().iterator().next().getMessage())
                         .build()
                 );
     }
