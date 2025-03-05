a4c6b5fe2920e28549843d3e8690594e25908987
Feigla <bogdansimon0v@mail.ru>
2025-03-05 22:27:26 +0300

    filled errorResponse and put it in metadata for handling exceptions

--- profile/src/main/java/ru/bogdsvn/profile/services/grpc/GrpcProfileServerService.java
+++ profile/src/main/java/ru/bogdsvn/profile/services/grpc/GrpcProfileServerService.java
@@ -1,9 +1,13 @@
 package ru.bogdsvn.profile.services.grpc;
 
+import io.grpc.Metadata;
+import io.grpc.Status;
+import io.grpc.protobuf.ProtoUtils;
 import io.grpc.stub.StreamObserver;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.springframework.http.HttpStatus;
 import org.springframework.transaction.annotation.Transactional;
 import ru.bogdsvn.grcp.profile.ProfileOuterClass;
 import ru.bogdsvn.grcp.profile.ProfileServiceGrpc;
@@ -26,6 +30,18 @@ public class GrpcProfileServerService extends ProfileServiceGrpc.ProfileServiceI
     public void getPreference(ProfileOuterClass.Profile request, StreamObserver<ProfileOuterClass.Preference> responseObserver) {
         PreferenceEntity entity = preferenceRepository.findById(request.getUserId()).orElse(null);
 
+        if (entity == null) {
+            Metadata.Key<ProfileOuterClass.ErrorResponse> errorResponseKey = ProtoUtils
+                    .keyForProto(ProfileOuterClass.ErrorResponse.getDefaultInstance());
+            ProfileOuterClass.ErrorResponse error = ProfileOuterClass.ErrorResponse.newBuilder()
+                    .setMessage("Заполните свои интересы")
+                    .build();
+            Metadata metadata = new Metadata();
+            metadata.put(errorResponseKey, error);
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Не заполнены интересы").asRuntimeException(metadata));
+            return;
+        }
+
         ProfileOuterClass.Preference preference  = ProfileOuterClass.Preference
                 .newBuilder()
                 .setGender(entity.getGender().value)
@@ -40,6 +56,11 @@ public class GrpcProfileServerService extends ProfileServiceGrpc.ProfileServiceI
     @Override
     public void getBio(ProfileOuterClass.Profile request, StreamObserver<ProfileOuterClass.Bio> responseObserver) {
         BioEntity entity = bioRepository.findById(request.getUserId()).orElse(null);
+
+        if (entity == null) {
+            responseObserver.onError(Status.NOT_FOUND.withDescription("Не заполнен профиль").asRuntimeException());
+            return;
+        }
 
         ProfileOuterClass.Bio bio  = ProfileOuterClass.Bio
                 .newBuilder()
