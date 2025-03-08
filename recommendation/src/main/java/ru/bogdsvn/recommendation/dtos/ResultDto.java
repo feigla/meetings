03c23a8cccdf42132f1eae42cd1a1b0d473f6511
Feigla <bogdansimon0v@mail.ru>
2025-03-08 11:35:17 +0300

    fixed bug with json ignore

--- recommendation/src/main/java/ru/bogdsvn/recommendation/dtos/ResultDto.java
+++ recommendation/src/main/java/ru/bogdsvn/recommendation/dtos/ResultDto.java
@@ -1,10 +1,10 @@
 package ru.bogdsvn.recommendation.dtos;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import net.minidev.json.annotate.JsonIgnore;
 
 @Builder
 @AllArgsConstructor
