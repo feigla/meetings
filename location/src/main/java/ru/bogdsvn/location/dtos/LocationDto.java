933b16d983875ebcbffa28d206eb654e32b8d4a3
Feigla <bogdansimon0v@mail.ru>
2025-03-08 23:28:44 +0300

    validated location dto is not null

--- location/src/main/java/ru/bogdsvn/location/dtos/LocationDto.java
+++ location/src/main/java/ru/bogdsvn/location/dtos/LocationDto.java
@@ -3,6 +3,7 @@ package ru.bogdsvn.location.dtos;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -16,10 +17,12 @@ public class LocationDto {
     @JsonProperty("latitude")
     @Min(value = 0, message = "Широта имеет значения от 0 градусов до 90 градусов")
     @Max(value = 90, message = "Широта имеет значения от 0 градусов до 90 градусов")
+    @NotNull
     private Double latitude;
 
     @JsonProperty("longitude")
     @Min(value = 0, message = "Долгота имеет значения от 0 градусов до 180 градусов")
     @Max(value = 180, message = "Долгота имеет значения от 0 градусов до 180 градусов")
+    @NotNull
     private Double longitude;
 }
