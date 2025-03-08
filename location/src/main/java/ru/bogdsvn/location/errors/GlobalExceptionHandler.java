a01a10c77eef9b89a589fea54c56c4ae05699169
Feigla <bogdansimon0v@mail.ru>
2025-03-08 22:51:17 +0300

    added handling exceptions

--- location/src/main/java/ru/bogdsvn/location/errors/GlobalExceptionHandler.java
+++ location/src/main/java/ru/bogdsvn/location/errors/GlobalExceptionHandler.java
@@ -18,4 +18,26 @@ public class GlobalExceptionHandler {
                         .build()
                 );
     }
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorDto> handleNotFoundException(NotFoundException ex) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(ErrorDto.builder()
+                        .error(HttpStatus.NOT_FOUND.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .build()
+                );
+    }
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<ErrorDto> handleBadRequestException(BadRequestException ex) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(ErrorDto.builder()
+                        .error(HttpStatus.BAD_REQUEST.getReasonPhrase())
+                        .message(ex.getMessage())
+                        .build()
+                );
+    }
 }
