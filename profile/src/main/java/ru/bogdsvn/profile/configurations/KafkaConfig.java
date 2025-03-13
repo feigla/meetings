5abe0110e1e1639d029fae6b3fc4882bad380d39
Feigla <bogdansimon0v@mail.ru>
2025-03-13 23:03:18 +0300

    added kafka configurations

--- profile/src/main/java/ru/bogdsvn/profile/configurations/KafkaConfig.java
+++ profile/src/main/java/ru/bogdsvn/profile/configurations/KafkaConfig.java
@@ -1,40 +1,48 @@
 package ru.bogdsvn.profile.configurations;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.config.TopicBuilder;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.core.*;
+import org.springframework.kafka.support.serializer.JsonSerializer;
 
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
 @Configuration
 public class KafkaConfig {
+    @Value("${location.command.topic}")
+    private String locationCommandTopic;
+
+    @Value("${recommendation.command.topic}")
+    private String recommendationCommandTopic;
+
     @Bean
     public KafkaTemplate<String, Object> kafkaTemplate(ProducerFactory<String, Object> producerFactory) {
         return new KafkaTemplate<>(producerFactory);
     }
 
     @Bean
-    public NewTopic createProfileEventsTopic() {
-        return TopicBuilder.name("recommendation-service")
-                .partitions(5)
-                .replicas(2)
+    public NewTopic createLocationCommandTopic() {
+        return TopicBuilder.name(locationCommandTopic)
+                .partitions(1)
+                .replicas(1)
                 .build();
     }
 
     @Bean
-    public NewTopic createLocationServiceTopic() {
-        return TopicBuilder.name("location-service")
-                .partitions(5)
-                .replicas(2)
-                .build();
-    }
-
-    @Bean
-    public NewTopic createProfileServiceTopic() {
-        return TopicBuilder.name("profile-service")
-                .partitions(5)
-                .replicas(2)
+    public NewTopic createRecommendationCommandTopic() {
+        return TopicBuilder.name(recommendationCommandTopic)
+                .partitions(1)
+                .replicas(1)
                 .build();
     }
 }
