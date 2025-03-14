8715beae71b324b358582f363832d87e04f13ea4
Feigla <bogdansimon0v@mail.ru>
2025-03-14 20:55:59 +0300

    added events

---
+++ kafka-library/src/main/java/ru/bogdsvn/kafka_library/events/ProcessedLocationEvent.java
@@ -0,0 +1,10 @@
+package ru.bogdsvn.kafka_library.events;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProcessedLocationEvent {
+    private Long id;
+}
---
+++ kafka-library/src/main/java/ru/bogdsvn/kafka_library/events/ProcessedRecommendationEvent.java
@@ -0,0 +1,10 @@
+package ru.bogdsvn.kafka_library.events;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProcessedRecommendationEvent {
+    private Long id;
+}
