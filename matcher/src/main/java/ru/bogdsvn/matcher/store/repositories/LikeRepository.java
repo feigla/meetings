7d3ad432b2f61efbe1773d95c96ae389728ad8e4
Feigla <bogdansimon0v@mail.ru>
2025-03-31 11:35:19 +0300

    added repository

--- matcher/src/main/java/ru/bogdsvn/matcher/store/repositories/LikeRepository.java
+++ matcher/src/main/java/ru/bogdsvn/matcher/store/repositories/LikeRepository.java
@@ -1,10 +1,19 @@
 package ru.bogdsvn.matcher.store.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.bogdsvn.matcher.store.LikeEmbeddedId;
 import ru.bogdsvn.matcher.store.entities.LikeEntity;
 
 @Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, LikeEmbeddedId> {
+    @Modifying
+    @Query("""
+    DELETE FROM LikeEntity e
+    WHERE e.id = :id
+    """)
+    void deleteById(@Param("id") LikeEmbeddedId id);
 }
